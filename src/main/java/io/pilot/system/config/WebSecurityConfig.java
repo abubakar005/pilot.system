d88e42eed3aa4c7a5101fe3cc9d11e67d496bcb1
Abu Bakar <abubakar2@telenorbank.pk>
2022-06-26 18:38:37 +0500

    Updated some security configurations

--- src/main/java/io/pilot/system/config/WebSecurityConfig.java
+++ src/main/java/io/pilot/system/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package io.pilot.system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,24 +13,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value( "${auth.user.name}" )
+    private String USER_NAME;
+
+    @Value( "${auth.user.pass}" )
+    private String USER_PASS;
+
+    @Value( "${auth.user.role}" )
+    private String USER_ROLE;
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        String encodedPassword = encoder.encode("admin");
+        String encodedPassword = encoder.encode(USER_PASS);
         auth.inMemoryAuthentication()
-                .withUser("admin").password(encodedPassword).roles("Admin");
+                .withUser(USER_NAME).password(encodedPassword).roles(USER_ROLE);
     }
 
     @Override
     protected void configure(HttpSecurity http)throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("Admin")
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("**/swagger-ui/**").permitAll()
+                .anyRequest()
+                .fullyAuthenticated()
                 .and()
-                .httpBasic()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403");
+                .httpBasic();
+
+        http.csrf().disable();
     }
 }
